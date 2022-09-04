234a922f9dc1d2751ff604185d0d49897d121991
kapoor62 <98938389+kapoor62@users.noreply.github.com>
2022-09-04 16:44:36 -0400

    Initial commit

---
+++ reversewords.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseWords(String s) {
+        //https://www.youtube.com/watch?v=vhnRAaJybpA
+        int i=0;
+        int n = s.length();
+        String result = "";
+
+        while (i<n){
+            while(i<n && s.charAt(i)==' ') i++;
+            if (i>=n) break; // for last space
+            int j= i+1;
+            while (j<n && s.charAt(j) !=' ') j++;
+            String word = s.substring(i,j);
+            if (result.length()==0) result= word; //for first word
+            else result = word +' ' + result;
+            i = j+1;
+
+        }
+        return result;
+
+
+
+
+    }
+}
